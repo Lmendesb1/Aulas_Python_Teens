6318ba45869ecc14a1ed8743081c86e82f03c148
Lmendesb1 <lucasprata2010@gmail.com>
2025-02-01 15:17:22 +0000

    Aulas Iniciais

--- Aula2.py
+++ Aula2.py
@@ -3,10 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Buttons and Labels Example")
 
-button1 = tk.Button(root, text="Clique Aqui")
-button1.pack(pady=10)
+button1 = tk.Button(root, text="PADY")
+button1.pack(pady=100)
 
-button2 = tk.Button(root, text="Clique Aqui")
+button2 = tk.Button(root, text="PLACE")
 button2.place(x=100, y=50)
 
 
